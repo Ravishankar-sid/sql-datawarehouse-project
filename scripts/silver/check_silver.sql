c6a76e06ec2434a999a23c875ccab49ee97eaead
Ravishankar Siddula <53685729+Ravishankar-sid@users.noreply.github.com>
2025-03-23 14:33:23 +0530

    Create check_silver.sql

---
+++ scripts/silver/check_silver.sql
@@ -0,0 +1,7 @@
+/*
+====================================================================
+Script Purpose:
+====================================================================
+    This stored procedure performs check and analyse the data  
+    populate the tables from the 'bronze' schema.
+*/
